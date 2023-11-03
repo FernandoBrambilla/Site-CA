2cec6c54aa97d09db614bf6f415133286de89500
Fernando <fernandobrambillab1@hotmail.com>
2023-11-03 16:26:40 -0300

    responsivity

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 const backToTop = () => {
-    if (window.scrollY > 100) 
+    if (window.scrollY > 20) 
         $("#back-to-top").css("visibility","visible");
-    if (window.scrollY <= 100) 
+    if (window.scrollY <= 19) 
         $("#back-to-top").css("visibility","hidden");
     }
 window.addEventListener('scroll', function () {
